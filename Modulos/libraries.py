a2af63d6849a30fdd7a5bda847e3c30d15485fdb
ViitoDev <joaovictorpiresdev@gmail.com>
2025-12-01 19:30:22 -0300

    Using property

--- Modulos/libraries.py
+++ Modulos/libraries.py
@@ -3,17 +3,26 @@ class Library:
 
     def __init__(self, name):
         self.name = name
-        self.active = False
+        self._active = False
         Library.libraries.append(self)
 
     def __str__(self):
         return self.name
     
-    def listLibraries():
+    def list_Libraries():
         for library in Library.libraries:
             print(f"{library.name} | {library.active}")
+
+    def toggle_state(self):
+        self._active = not self._active
+
+    @property
+    def active(self):
+        "Ativada" if self._active else "Desativada"
     
 city_library = Library("City Library")
+city_library.toggle_state()
 shopping_library = Library("Shopping Library")
+shopping_library.toggle_state()
 
-Library.listLibraries()
+Library.list_Libraries()
